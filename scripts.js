5bfef1b5620f53733732db926c6778d2b9871701
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2025-03-20 17:23:40 +0300

    Add parallax effect

---
+++ scripts.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let parallaxSection = document.querySelector('.parallax')
+    if (parallaxSection) {
+        let clouds = document.querySelector('.images_clouds')
+        let mountains = document.querySelector('.images_mountains')
+        let people = document.querySelector('.images_human')
+    
+        let forClouds = 40
+        let forMountains = 20
+        let forHuman = 10
+        let animationSpeed = 0.05
+        let positionX = 0
+        let positionY = 0
+        let coordXpercent = 0
+        let coordYpercent = 0
+    
+        function setParalaxAnimation() {
+            let distX = coordXpercent - positionX
+            let distY = coordYpercent - positionY
+            positionX = positionX + distX * animationSpeed
+            positionY = positionY + distY * animationSpeed
+    
+            clouds.style.cssText = `transform: translate(${positionX/forClouds}%, ${positionY/forClouds}%`
+            mountains.style.cssText = `transform: translate(${positionX/forMountains}%, ${positionY/forMountains}%`
+            people.style.cssText = `transform: translate(${positionX/forHuman}%, ${positionY/forHuman}%`
+            requestAnimationFrame(setParalaxAnimation)
+        }
+        setParalaxAnimation()
+    
+        parallaxSection.addEventListener("mousemove", (event) => {
+            rect = parallaxSection.getBoundingClientRect()
+            parallaxWidth = rect.width
+            parallaxHeight = rect.height
+            x = event.pageX
+            y = event.pageY
+            coordX = x - parallaxWidth / 2
+            coordY = y - parallaxHeight / 2
+            coordXpercent = coordX / parallaxWidth * 100
+            coordYpercent = coordY / parallaxHeight * 100
+        })
+    }
+})
