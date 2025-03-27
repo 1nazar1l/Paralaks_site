eff24d85b729f8ed41cbc0db5531e6ba3755d314
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2025-03-27 17:19:04 +0300

    Update code

--- scripts.js
+++ scripts.js
@@ -1,65 +1,67 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let parallaxSection = document.querySelector('.parallax')
-    if (parallaxSection) {
-        let clouds = document.querySelector('.images_clouds')
-        let mountains = document.querySelector('.images_mountains')
-        let people = document.querySelector('.images_human')
-        let content = document.querySelector('.content')
-        let forClouds = 40
-        let forMountains = 20
-        let forHuman = 10
-        let animationSpeed = 0.05
-        let positionX = 0
-        let positionY = 0
-        let coordXpercent = 0
-        let coordYpercent = 0
+    const parallax = document.querySelector('.parallax');
+
+    if (parallax) {
+
+        const content = document.querySelector('.parallax__content');
+        const clouds = document.querySelector('.images__clouds');
+        const mountains = document.querySelector('.images__mountains');
+        const human = document.querySelector('.images__human');
+
+        const forClouds = 40;
+        const forMountains = 20;
+        const forHuman = 10;
+
+        const animationSpeed = 0.05;
+
+        let positionX = 0;
+        let positionY = 0;
+        let coordXpercent = 0;
+        let coordYpercent = 0;
     
         function setParalaxAnimation() {
-            console.log("coordX: ", coordXpercent)
-            console.log("posX: ", positionX)
-            let distX = coordXpercent - positionX
-            console.log("distX: ", distX)
-            let distY = coordYpercent - positionY
-            positionX = positionX + distX * animationSpeed
-            positionY = positionY + distY * animationSpeed
-    
-            clouds.style.cssText = `transform: translate(${positionX/forClouds}%, ${positionY/forClouds}%`
-            mountains.style.cssText = `transform: translate(${positionX/forMountains}%, ${positionY/forMountains}%`
-            people.style.cssText = `transform: translate(${positionX/forHuman}%, ${positionY/forHuman}%`
-            requestAnimationFrame(setParalaxAnimation)
-        }
-        setParalaxAnimation()
+            const distX = coordXpercent - positionX;
+            const distY = coordYpercent - positionY;
 
-        parallaxSection.addEventListener("mousemove", function(event) {
-            const parallaxWidth = parallaxSection.offsetWidth;
-            const parallaxHeight = parallaxSection.offsetHeight;
-            let x = event.pageX
-            let y = event.pageY
-            let coordX = x - parallaxWidth / 2
-            let coordY = y - parallaxHeight / 2
-            coordXpercent = coordX / parallaxWidth * 100
-            coordYpercent = coordY / parallaxHeight * 100
+            positionX = positionX + distX * animationSpeed;
+            positionY = positionY + distY * animationSpeed;
+    
+            clouds.style.cssText = `transform: translate(${positionX/forClouds}%, ${positionY/forClouds}%);`;
+            mountains.style.cssText = `transform: translate(${positionX/forMountains}%, ${positionY/forMountains}%);`;
+            human.style.cssText = `transform: translate(${positionX/forHuman}%, ${positionY/forHuman}%);`;
+            requestAnimationFrame(setParalaxAnimation);
+        }
+        setParalaxAnimation();
+
+        parallax.addEventListener("mousemove", function(event) {
+            const parallaxWidth = parallax.offsetWidth;
+            const parallaxHeight = parallax.offsetHeight;
+
+            const coordX = event.pageX - parallaxWidth / 2;
+            const coordY = event.pageY - parallaxHeight / 2;
+
+            coordXpercent = coordX / parallaxWidth * 100;
+            coordYpercent = coordY / parallaxHeight * 100;
         })
 
-        let thresholdSets =  []
-        for (let i = 0; i <= 1; i+=0.005) {
-            thresholdSets.push(i)
+        let thresholdSets =  [];
+        for (let i = 0; i <= 1.0; i+=0.005) {
+            thresholdSets.push(i);
         }
 
         function setParallaxStyle(scrollTopPercent) {
-            content.style.cssText = `transform: translate(0%, -${scrollTopPercent / 9}%)`
-            mountains.parentElement.style.cssText = `transform: translate(0%, -${scrollTopPercent / 6}%)`
-            people.parentElement.style.cssText = `transform: translate(0%, -${scrollTopPercent / 3}%)`
+            content.style.cssText = `transform: translate(0%, -${scrollTopPercent / 9}%);`;
+            mountains.parentElement.style.cssText = `transform: translate(0%, -${scrollTopPercent / 6}%);`;
+            human.parentElement.style.cssText = `transform: translate(0%, -${scrollTopPercent / 3}%);`;
         }
 
         const callback = (entries, observer) => {
-            const parallaxHeight = parallaxSection.offsetHeight;
-            let scrollTopPercent = (window.pageYOffset / parallaxHeight) * 100
-            setParallaxStyle(scrollTopPercent)
+            const scrollTopPercent = (window.pageYOffset / parallax.offsetHeight) * 100;
+            setParallaxStyle(scrollTopPercent);
         }
         const observer = new IntersectionObserver(callback, {
             threshold: thresholdSets 
         });
-        observer.observe(content)
+        observer.observe(document.querySelector('.content'))
     }
 })
